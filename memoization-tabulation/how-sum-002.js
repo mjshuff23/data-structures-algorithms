671866ab18badabe2b57015f7c18c5790b07fa1f
Michael Shuff <mjshuff23@gmail.com>
2021-03-31 23:13:05 -0400

    added howsum

---
+++ memoization-tabulation/how-sum-002.js
@@ -0,0 +1,20 @@
+// m - targetSum, n - numbers.length
+// Time - O(n * m^2), Space - O(m^2)
+function howSum(targetSum, numbers, memo = {}) {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  for (const num of numbers) {
+    const remainder = targetSum - num;
+    const remainderResult = howSum(remainder, numbers, memo);
+    if (remainderResult) {
+      memo[targetSum] = [...remainderResult, num];
+      return memo[targetSum];
+    }
+  }
+  memo[targetSum] = null;
+  return memo[targetSum];
+}
+
+console.log(howSum(3002, [29, 17, 50, 12]));
---
+++ memoization-tabulation/how-sum-01.js
@@ -0,0 +1,15 @@
+function howSum(targetSum, numbers) {
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  for (const num of numbers) {
+    const remainder = targetSum - num;
+    const remainderResult = howSum(remainder, numbers);
+    if (remainderResult) {
+      return [...remainderResult, num];
+    }
+  }
+  return null;
+}
+
+console.log(howSum(300, [7, 14]));
