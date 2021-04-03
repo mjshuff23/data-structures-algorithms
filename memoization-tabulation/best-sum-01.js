01c26399bc3afc8d831559d7c754fe1d26b83c5b
Michael Shuff <mjshuff23@gmail.com>
2021-04-03 19:22:24 -0400

    added bestSum

--- memoization-tabulation/best-sum-01.js
+++ memoization-tabulation/best-sum-01.js
@@ -4,7 +4,32 @@
 
 // If there is a tie for the shortest combination, you may return any one of the shortest
 
-function bestSum(targetSum, numbers) {}
+// m = targetSum
+// n = numbers.length
 
-console.log(bestSum(7, [4, 3, 7]));
-console.log(bestSum(214, [5, 9, 20, 15, 17, 3, 1]));
+// Time - O(n^m * m)
+// Space - O(m^2)
+function bestSum(targetSum, numbers) {
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  let shortestCombo = null;
+
+  // Iterates and attempts all branches
+  for (const num of numbers) {
+    const remainder = targetSum - num;
+    const remainderCombo = bestSum(remainder, numbers);
+    if (remainderCombo) {
+      const combination = [...remainderCombo, num];
+      if (!shortestCombo || combination.length < shortestCombo.length) {
+        shortestCombo = combination;
+      }
+    }
+  }
+
+  return shortestCombo;
+}
+
+console.log(bestSum(7, [4, 3, 7])); // [7]
+console.log(bestSum(100, [1, 2, 5, 25])); // [25, 25, 25, 25]
+console.log(bestSum(214, [5, 9, 20, 15, 17, 3, 1])); // null
