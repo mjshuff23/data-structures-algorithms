c077b049789e9dbd70339641388b4f571cab307a
Michael Shuff <mjshuff23@gmail.com>
2021-04-04 18:34:55 -0400

    added stack

---
+++ data-structures/stack.js
@@ -0,0 +1,65 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+
+    if (this.length) {
+      let currentTop = this.top;
+      this.top = newNode;
+      newNode.next = currentTop;
+      this.length++;
+
+      return this.length;
+    }
+
+    this.top = newNode;
+    // increment and return
+    return ++this.length;
+  }
+
+  pop() {
+    if (!this.length) return null;
+
+    if (this.length === 1) {
+      let result = this.top;
+      this.top = null;
+      this.length--;
+
+      return result.value;
+    }
+
+    let result = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return result.value;
+  }
+
+  size() {
+    return this.length;
+  }
+}
+
+let node1 = new Node('test');
+let node2 = new Node('test2');
+node2.next = node1;
+let stack = new Stack();
+stack.push(node2);
+// console.log(stack);
+let result = stack.pop(); // 'test2'
+console.log(result);
+
+module.exports = {
+  Node,
+  Stack,
+};
